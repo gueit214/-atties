551c994d60a32a2bb0b6d91c261d6671e1b5cada
Kuesung Park <gueit214@naver.com>
2023-05-25 10:34:21 +0900

    feat : Meta tag 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,44 @@ import type { Metadata } from 'next';
 import './global.css';
 
 export const metadata: Metadata = {
-  title: 'Atties',
+  title: {
+    default: 'Atties',
+    template: '%s | Atties',
+  },
   description: 'Welcome to Atties !',
+  openGraph: {
+    title: 'Atties',
+    description: 'Welcome to Atties !',
+    url: 'https://atties.vercel.app',
+    siteName: 'Atties',
+    images: [
+      {
+        url: 'https://atties.vercel.app',
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'ko-KR',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: 'Atties',
+    card: 'summary_large_image',
+  },
+  icons: {
+    shortcut: '/static/favicon.ico',
+  },
 };
 
 export default function RootLayout({
