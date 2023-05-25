83a11cf18e8e314fad4ad336fba2341795b533c2
Kuesung Park <gueit214@naver.com>
2023-05-25 09:44:11 +0900

    feat : tailwind에 app라우팅 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
